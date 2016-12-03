83752ab4a317aa5c1d2af366d0d23afd7acbb2db
hasan_cse91 <hasan_cse91@yahoo.com>
2016-12-03 21:46:33 +0600

    RetrofitApiClient updated. now it is real Singleton

--- Android Code/app/src/main/java/com/hellohasan/networkcallwithretrofit/Retrofit/RetrofitApiClient.java
+++ Android Code/app/src/main/java/com/hellohasan/networkcallwithretrofit/Retrofit/RetrofitApiClient.java
@@ -1,22 +1,20 @@
-package com.hellohasan.networkcallwithretrofit.Retrofit;
-
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-
+ 
 public class RetrofitApiClient {
-
+ 
         private static final String BASE_URL = "http://192.168.0.101"; //address of your localhost
         private static Retrofit retrofit = null;
-
+ 
         private static Gson gson = new GsonBuilder()
                 .setLenient()
                 .create();
 
-        public static Retrofit getClient() {
+        private RetrofitApiClient() {} // So that nobody can create an object with constructor
+ 
+        public static synchronized Retrofit getClient() {
             if (retrofit==null) {
                 retrofit = new Retrofit.Builder()
                         .baseUrl(BASE_URL)
@@ -25,7 +23,5 @@ public class RetrofitApiClient {
             }
             return retrofit;
         }
-
+ 
 }
-
-
